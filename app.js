d8bf2b19753c1073b96b54df09818bcf102c1322
karan <karansuthar.hm@gmail.com>
2024-09-23 18:09:27 +0530

    Add all files

---
+++ app.js
@@ -0,0 +1,18 @@
+const play_pause = document.getElementById("play-pause");
+const audio = new Audio("audio/vipul.mp3");
+
+let isFirstImage = true
+
+play_pause.addEventListener("click", function() {
+    if (isFirstImage) {
+      play_pause.src = "imgs/Pause.png";
+       audio.play();
+      audio.loop = "loop"
+    } else {
+      play_pause.src = "imgs/Play.png";
+      audio.pause();
+      audio.currentTime = 0;
+      audio.src = audio.src;
+    }
+    isFirstImage = !isFirstImage;
+  });
