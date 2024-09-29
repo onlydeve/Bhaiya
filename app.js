39c5d9c03957d98a2527c60c51c40622031672fe
karan <karansuthar.hm@gmail.com>
2024-09-29 15:59:24 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const play_pause = document.getElementById("play-pause");
 const audio = new Audio("audio/vipul.mp3");
 
-let isFirstImage = true;
+let isPlaying = false;
 
 play_pause.addEventListener("click", () => {
-  if (isFirstImage) {
+  if (!isPlaying) {
     play_pause.src = "imgs/Pause.png";
     audio.play();
-    audio.loop = "loop";
+    // audio.loop = "loop";
   } else {
     play_pause.src = "imgs/Play.png";
     audio.pause();
     audio.currentTime = 0;
-    audio.src = audio.src;
   }
-  isFirstImage = !isFirstImage;
+  isPlaying = !isPlaying;
+});
+
+audio.addEventListener("ended", () => {
+  play_pause.src = "imgs/Play.png";
+  isPlaying = false;
 });
