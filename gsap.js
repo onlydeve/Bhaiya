ee0dc8dad5cd6713c98c6a9b8349c0cc1df86474
karan <karansuthar.hm@gmail.com>
2024-09-30 14:58:12 +0530

    gsap easing

--- gsap.js
+++ gsap.js
@@ -1,21 +1,22 @@
 gsap.from("#play-pause", {
-    opacity: 0,
-    y: -30,
-    duration: 0.3,
-    delay: 0.5,
-  });
+  opacity: 0,
+  y: -30,
+  duration: 0.3,
+  delay: 0.5,
+});
 
-  gsap.from("#logo", {
-    opacity: 0,
-    y: -30,
-    duration: 0.3,
-    delay: 0.5,
-  });
+gsap.from("#logo", {
+  opacity: 0,
+  y: -30,
+  duration: 0.3,
+  delay: 0.5,
+});
 
-  gsap.from(".para", {
-    opacity: 0,
-    y: -30,
-    duration: 0.3,
-    delay: 0.8,
-    stagger: 0.1,
-  });
+gsap.from(".para", {
+  opacity: 0,
+  duration: 0.7,
+  ease: "back.out(1.7)",
+  y: -250,
+  delay: 0.8,
+  stagger: 0.15,
+});
